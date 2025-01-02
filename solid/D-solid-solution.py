7884ca78b95b4d7f68181d0725af4fbd5bafc2a1
carlos <carlosartica40@gmail.com>
2025-01-02 01:08:32 -0500

    code modification exercise 1,2

--- solid/D-solid-solution.py
+++ solid/D-solid-solution.py
@@ -1,1 +1,17 @@
 # Here your solution
+# Implementando "Unica responsabilidad"
+
+class PayPalService:
+    @staticmethod
+    def pay(amount: float):
+        print(f"Paying {amount} using PayPal...")
+
+# separamos la creación del servicio de pago del procesamiento
+
+
+class PaymentProcessor:
+    def __init__(self, pay_service):
+        self.pay_service = pay_service
+
+    def process_payment(self, amount: float):
+        self.pay_service.pay(amount)
--- solid/I-solid-solution.py
+++ solid/I-solid-solution.py
@@ -1,1 +1,23 @@
 # Here your solution
+
+from abc import ABC, abstractmethod
+
+# Creamos interfaces que se necesiten(Interfaces específicas)
+
+
+class IPaymentGateway(ABC):
+    @abstractmethod
+    def pay(self, amount: float) -> None:
+        pass
+
+
+class Refunds(ABC):
+    @abstractmethod
+    def refund(self, amount: float) -> None:
+        pass
+
+
+class Handlerdispute(ABC):
+    @abstractmethod
+    def handle_dispute(self, dispute_id: str) -> None:
+        pass
